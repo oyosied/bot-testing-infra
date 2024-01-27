f4a4ec3ca77ef248f24486823eb4f24b40feebdf
Yossi Ioukhtov <vrectrox@gmail.com>
2024-01-28 00:30:25 +0200

    fix(db handler): name changes and bug fixes

--- database/db_handler.py
+++ database/db_handler.py
@@ -2,6 +2,8 @@ import json
 import os
 from typing import List
 
+from marshmallow import ValidationError
+
 
 def commit_changes(func):
     def open_and_save_data(self, *arg, **kw):
@@ -19,10 +21,11 @@ def commit_changes(func):
 
 class DatabaseHandler:
 
-    def __init__(self, path_to_json, field_map, unique_fields):
+    def __init__(self, path_to_json, field_map, unique_fields, primary_search_key):
         self.path_to_json = path_to_json
         self.field_map = field_map
         self.unique_fields = ['id', *unique_fields]
+        self.primary_search_key = primary_search_key
         self.validate_path()
 
     def validate_path(self):
@@ -48,11 +51,11 @@ class DatabaseHandler:
         return value in data["unique_fields"][field_name]
 
     def check_all_unique_fields_exists(self, data, item):
-        non_unique_fields = {}
+        unique_fields = {}
         for unique_field in self.unique_fields:
             if self.unique_field_exists(data, item[unique_field], unique_field):
-                non_unique_fields[unique_field] = item[unique_field]
-        return non_unique_fields if non_unique_fields else False
+                unique_fields[unique_field] = item[unique_field]
+        return unique_fields if unique_fields else False
 
     def add_unique_field(self, data, value, field_name):
         data["unique_fields"][field_name].append(value)
@@ -64,14 +67,14 @@ class DatabaseHandler:
         new_item = {'id': current_id, **new_item}
         existing_values = self.check_all_unique_fields_exists(data=data, item=new_item)
         if existing_values:
-            raise Exception(f"Could not add '{new_item}', "
+            raise ValidationError(f"Could not add '{new_item}', "
                             f"error: 'The following values already exist '{existing_values}'")
 
-        data["items"][new_item['name']] = new_item
+        data["items"][new_item[self.primary_search_key]] = new_item
         data["current_id"] += 1
         for field in self.unique_fields:
             self.add_unique_field(data, new_item[field], field)
-        return current_id
+        return new_item[self.primary_search_key]
 
     def remove_unique_field_value(self, data, value, field_name):
         data["unique_fields"][field_name].remove(value)
@@ -96,9 +99,30 @@ class DatabaseHandler:
         if not item_id or item_id == '':
             return "Please provide an ID"
         try:
-            try:
+            if self.primary_search_key in updated_fields:
+                old_data = {**data["items"][item_id]}
+                new_item = {**data["items"][item_id]}
+                new_item.update(updated_fields)
+                existing_values = self.check_all_unique_fields_exists(data=data, item=new_item)
+
+                if self.primary_search_key in existing_values:
+
+                    if old_data[self.primary_search_key] != new_item[self.primary_search_key]:
+                        raise ValidationError(f"Could not add '{new_item}', "
+                                              f"error: 'The following values already exist '{existing_values}'")
+
+                    elif old_data[self.primary_search_key] == new_item[self.primary_search_key]:
+                        data["items"][item_id].update(updated_fields)
+
+                    else:
+                        data["items"][item_id] = new_item
+                        data["items"][new_item[self.primary_search_key]] = data["items"].pop(item_id)
+                        primary_key_index = data["unique_fields"][self.primary_search_key].index(old_data[self.primary_search_key])
+                        data["unique_fields"][self.primary_search_key][primary_key_index] = new_item[self.primary_search_key]
+            else:
                 data["items"][item_id].update(updated_fields)
-            except KeyError as e:
-                raise KeyError(f"Couldn't find '{item_id}', skipping ....")
+
+        except KeyError as e:
+            raise KeyError(f"Couldn't find '{item_id}', skipping ....")
         except Exception as e:
             raise Exception(f"Could not update '{item_id}', error:{e}")
