b480e8dde8ba4630be3a11e066151feb94b3eac8
Yossi Ioukhtov <vrectrox@gmail.com>
2024-01-28 00:28:18 +0200

    chore(models): changes in argument names

--- bot_controller.py
+++ bot_controller.py
@@ -9,20 +9,20 @@ bot_blueprint = Blueprint('bot_blueprint', __name__)
 bot = Bot()
 
 
-@bot_blueprint.route('/<bot_id>', methods=['GET'])
-def get_bot(bot_id):
+@bot_blueprint.route('/<bot_name>', methods=['GET'])
+def get_bot(bot_name):
     try:
-        bot_details = bot.get(bot_id=bot_id)
+        bot_details = bot.get(bot_name=bot_name)
         return jsonify(bot_details), 200
     except KeyError as e:
         return jsonify({'message': "Can't find bot"}), 404
     except Exception as e:
         return jsonify({'message': str(e)}), 404
 
-@bot_blueprint.route('/<bot_id>', methods=['DELETE'])
-def delete_bot(bot_id):
+@bot_blueprint.route('/<bot_name>', methods=['DELETE'])
+def delete_bot(bot_name):
     try:
-        bot.delete(bot_id=bot_id)
+        bot.delete(bot_name=bot_name)
         return '', 200
     except KeyError as e:
         return jsonify({'message': "Can't find bot"}), 404
@@ -36,8 +36,8 @@ def create_bot():
         bot_schema = BotSchema()
         json_data = request.get_json()
         result = bot_schema.load(json_data)
-        bot_id = bot.insert(new_bot=result)
-        return jsonify({'message': "Bot created successfully","bot_id": bot_id}), 200
+        bot_name = bot.insert(new_bot=result)
+        return jsonify({'message': "Bot created successfully","bot_primary_key": bot_name}), 200
 
     except ValidationError as e:
         return jsonify({'message': 'Validation error', 'errors': e.messages}), 400
@@ -46,15 +46,14 @@ def create_bot():
         return jsonify({'message': str(e)}), 404
 
 
-@bot_blueprint.route('/<bot_id>', methods=['PUT'])
-def update_bot(bot_id):
+@bot_blueprint.route('/<bot_name>', methods=['PUT'])
+def update_bot(bot_name):
     try:
         bot_schema = BotSchema(partial=True)
         json_data = request.get_json()
         result = bot_schema.load(json_data)
-        bot.update(bot_id=bot_id,updated_fields=result)
-        new_details = bot.get(bot_id=bot_id)
-        return jsonify({'message': "Bot updated successfully","updated_values": new_details}), 200
+        bot.update(bot_name=bot_name,updated_fields=result)
+        return jsonify({'message': "Bot updated successfully"}), 200
 
     except ValidationError as e:
         return jsonify({'message': 'Validation error', 'errors': e.messages}), 400
--- database/models/BotModel.py
+++ database/models/BotModel.py
@@ -2,11 +2,11 @@
 # This will act similarly
 import os
 
-from common.common import root_path
+from common.common import ROOT_PATH
 from database.db_handler import DatabaseHandler
 from database.models.BaseModel import BaseModel
 
-file_path = os.path.join(f"{root_path}\\database\\", 'bot.json')
+file_path = os.path.join(f"{ROOT_PATH}\\database\\", 'bot.json')
 field_map = ['name', 'url', 'intents']
 unique_fields = ['name']
 
@@ -14,16 +14,18 @@ unique_fields = ['name']
 class Bot(BaseModel):
     def __init__(self):
         self.db_handler: DatabaseHandler = DatabaseHandler(path_to_json=file_path, field_map=field_map,
-                                                           unique_fields=unique_fields)
+                                                           unique_fields=unique_fields, primary_search_key='name')
 
+    # bot's main index is the name, there is no search by ID
     def insert(self, new_bot):
         return self.db_handler.add_to_json(new_item=new_bot)
 
-    def update(self, bot_id, updated_fields):
-        self.db_handler.update_value(item_id=str(bot_id), updated_fields=updated_fields)
+    def update(self, bot_name, updated_fields):
+        self.db_handler.update_value(item_id=str(bot_name), updated_fields=updated_fields)
 
-    def delete(self, bot_id):
-        self.db_handler.remove_from_json(item_id=str(bot_id))
+    def delete(self, bot_name):
+        self.db_handler.remove_from_json(item_id=str(bot_name))
 
-    def get(self, bot_id):
-        return self.db_handler.get_specific_data(item_id=str(bot_id))
+    # will search by name of the bot
+    def get(self, bot_name):
+        return self.db_handler.get_specific_data(item_id=str(bot_name))
