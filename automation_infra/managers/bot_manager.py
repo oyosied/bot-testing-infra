8a424511643eab3d2882e1886c339c1355d3548b
oyosied <vrectrox@gmail.com>
2024-01-28 23:08:46 +0200

    fix(bot manager): update did not support switching names

--- automation_infra/managers/bot_manager.py
+++ automation_infra/managers/bot_manager.py
@@ -27,14 +27,16 @@ class BotManager:
         if response.status_code == 200:
             self.created_bots.append(bot_properties.name)
 
-    def update_bot(self, bot_properties: BotProperties, expected_status_code=200):
-        response = self.client.put(f"/bot/{bot_properties.name}", json=bot_properties.to_dict())
+    def update_bot(self, bot_name: str, bot_properties: BotProperties, expected_status_code=200):
+        response = self.client.put(f"/bot/{bot_name}", json=bot_properties.to_dict())
         logger.info(f"Request:'/bot'\n"
                     f"Response:{response.json}\n"
                     f"Body:{bot_properties.to_dict()}\n"
                     f"Status code:{response.status_code}")
         assert response.status_code == expected_status_code
-        self.created_bots.append(bot_properties.name)
+        if bot_name != bot_properties.name and response.status_code == 200:
+            self.created_bots.remove(bot_properties.name)
+            self.created_bots.append(bot_name)
 
     def delete_bot(self, bot_name, expected_status_code=200):
         response = self.client.delete(f"/bot/{bot_name}")
--- tests/bot_tests.py
+++ tests/bot_tests.py
@@ -35,11 +35,29 @@ class TestsBotAPI:
     def test_delete_bot_not_existent_name(self, bot_manager):
         bot_manager.delete_bot(bot_name=f"testing_{str(int(time.time() * 1000))}", expected_status_code=404)
 
+    def test_update_bot_bad_url(self, bot_manager):
+        bot_properties = BotProperties(name=f"testing_{str(int(time.time() * 1000))}", url="http://example.com",
+                                       intents=[random.choice(ALLOWED_INTENTS)])
+        bot_manager.create_bot(bot_properties=bot_properties)
+        bot_properties.url = ""
+        bot_manager.update_bot(bot_name=bot_properties.name, bot_properties=bot_properties, expected_status_code=400)
+
+    def test_update_bot_to_already_existing_name(self, bot_manager):
+        bot_properties_1 = BotProperties(name=f"testing1_{str(int(time.time() * 1000))}", url="http://example.com",
+                                         intents=[random.choice(ALLOWED_INTENTS)])
+        bot_properties_2 = BotProperties(name=f"testing2_{str(int(time.time() * 1000))}", url="http://example.com",
+                                         intents=[random.choice(ALLOWED_INTENTS)])
+        bot_manager.create_bot(bot_properties=bot_properties_1)
+        bot_manager.create_bot(bot_properties=bot_properties_2)
+
+        bot_manager.update_bot(bot_name=bot_properties_2.name, bot_properties=bot_properties_1,
+                               expected_status_code=400)
+
     def test_update_bot(self, bot_manager):
         bot_properties = BotProperties(name=f"testing_{str(int(time.time() * 1000))}", url="http://example.com",
                                        intents=[random.choice(ALLOWED_INTENTS)])
         bot_manager.create_bot(bot_properties=bot_properties)
-        bot_manager.update_bot(bot_properties=bot_properties)
+        bot_manager.update_bot(bot_name=bot_properties.name, bot_properties=bot_properties)
 
     def test_delete_bot(self, bot_manager):
         bot_properties = BotProperties(name=f"testing_{str(int(time.time() * 1000))}", url="http://example.com",
